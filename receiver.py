f07edc226a37a9e3d066b1732900ec87f02d39b9
mortelli <ortellimarcelo@gmail.com>
2020-11-04 15:41:43 -0300

    add receiver.py script

---
+++ receiver.py
@@ -0,0 +1,49 @@
+import sys
+
+from grpc import insecure_channel
+
+from api_pb2 import RskAddress, Msg, PublishPayload, Channel
+from api_pb2_grpc import CommunicationsApiStub
+
+
+def run(rif_comms_address, rsk_addr_to_use):
+    with insecure_channel(rif_comms_address) as channel:
+        print("connecting to comms node at", rif_comms_address)
+        stub = CommunicationsApiStub(channel)
+
+        rsk_addr = RskAddress(address=rsk_addr_to_use)
+        print("registering rsk address", rsk_addr.address)
+        stub.ConnectToCommunicationsNode(rsk_addr)
+
+        while True:
+            try:
+                peer_id = stub.LocatePeerId(rsk_addr).address
+                print("peer ID for rsk address is", peer_id)
+
+                print("creating topic for rsk address", rsk_addr.address)
+                topic = stub.CreateTopicWithRskAddress(rsk_addr)
+                topic_id = peer_id
+
+                print("awaiting responses for topic", topic_id)
+                for response in topic:
+                    print("got response %s for topic %s" % (response, topic_id))
+
+            except KeyboardInterrupt:
+                print("halting")
+
+                stub.SendMessageToTopic(
+                    PublishPayload(
+                        topic=Channel(channelId=topic_id),
+                        message=Msg(payload=str.encode("bye"))
+                    )
+                )
+
+                print("unsubscribing from topic", topic_id)
+                stub.CloseTopic(Channel(channelId=topic_id))
+                exit()
+
+
+if __name__ == "__main__":
+    node_address = sys.argv[1]
+    rsk_address = sys.argv[2]
+    run(node_address, rsk_address)
