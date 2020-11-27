0b6463294f3c011ec46ccebed9207c798394b6d8
mortelli <ortellimarcelo@gmail.com>
2020-11-27 15:18:49 -0300

    add check-subscription script plus needed utils functions

---
+++ check-subscription.py
@@ -0,0 +1,53 @@
+import sys
+
+from grpc import insecure_channel
+
+from api_pb2 import RskAddress
+from api_pb2_grpc import CommunicationsApiStub
+from utils import subscribe_to_topic, let_user_pick, is_subscribed_to
+
+addresses = [
+    "0x8cb891510dF75C223C53f910A98c3b61B9083c3B",
+    "0xeBfF0EEe8E2b6952E589B0475e3F0E34dA0655B1",
+    "0xbE165fe06c03e4387F79615b7A0b79d535e8D325",
+]
+
+
+def run(rif_comms_node_address: str, our_rsk_address: str):
+    with insecure_channel(rif_comms_node_address) as channel:
+        print("\nconnecting to comms node at", rif_comms_node_address)
+        stub = CommunicationsApiStub(channel)
+
+        our_rsk_addr = RskAddress(address=our_rsk_address)
+        print("registering rsk address", our_rsk_addr.address)
+        notification = stub.ConnectToCommunicationsNode(our_rsk_addr)
+
+        # subscribe to topics
+        while True:
+            try:
+                sub_option = let_user_pick(
+                    "pick a REGISTERED address to subscribe to. press ctrl-c when done.", addresses
+                ) - 1
+                subscribe_to_topic(stub, addresses[sub_option])
+            except KeyboardInterrupt:
+                break
+
+        # check subscriptions to topic
+        while True:
+            try:
+                sub_check_option = let_user_pick(
+                    "pick a REGISTERED address to check subscription to. press ctrl-c when done.", addresses
+                ) - 1
+                print(
+                    "is", our_rsk_address, "subscribed to", addresses[sub_check_option], "?",
+                    is_subscribed_to(our_rsk_address, addresses[sub_check_option])
+                )
+            except KeyboardInterrupt:
+                exit()
+
+
+if __name__ == "__main__":
+    comms_addr_param = sys.argv[1]
+    address_option = let_user_pick("pick an address to connect with:", addresses) - 1
+
+    run(comms_addr_param, addresses[address_option])
--- utils.py
+++ utils.py
@@ -1,16 +1,16 @@
 import json
 
-from api_pb2 import RskAddress, Notification, Channel
+from api_pb2 import RskAddress, Notification, Channel, Subscriber
 from api_pb2_grpc import CommunicationsApiStub
 
 
 def subscribe_to_topic(stub: CommunicationsApiStub, rsk_address: str) -> (Notification, str):
     rsk_addr = RskAddress(address=rsk_address)
 
-    print("subscribing to topic for rsk address", rsk_addr)
+    print("\nsubscribing to topic for rsk address", rsk_addr)
     topic = stub.CreateTopicWithRskAddress(rsk_addr)
-    topic_id = ""
 
+    topic_id = ""
     for response in topic:
         if response.channelPeerJoined.peerId:
             topic_id = response.channelPeerJoined.peerId
@@ -22,9 +22,26 @@ def subscribe_to_topic(stub: CommunicationsApiStub, rsk_address: str) -> (Notifi
     return topic, topic_id
 
 
+def is_subscribed_to(stub: CommunicationsApiStub, subscriber_address: str, topic_address: str) -> bool:
+    print("\nchecking subscription for", subscriber_address, "to topic ", topic_address)
+
+    our_peer_id = get_peer_id(stub, subscriber_address)
+    topic_id = get_peer_id(stub, topic_address)
+    return stub.HasSubscriber(
+        Subscriber(
+            peerId=our_peer_id,
+            channel=Channel(channelId=topic_id)
+        )
+    ).value
+
+
 def unsubscribe_from_topic(stub: CommunicationsApiStub, topic_id: str):
-    print("unsubscribing from topic", topic_id)
+    print("\nunsubscribing from topic", topic_id)
     stub.CloseTopic(Channel(channelId=topic_id))
+
+
+def get_peer_id(stub: CommunicationsApiStub, rsk_address: str) -> str:
+    return stub.LocatePeerId(RskAddress(address=rsk_address)).address
 
 
 def notification_to_message(notification: Notification) -> str:
@@ -36,3 +53,14 @@ def notification_to_message(notification: Notification) -> str:
     except AttributeError:
         pass
     return None
+
+
+def let_user_pick(options: []) -> int:
+    while True:
+        for idx, element in enumerate(options):
+            print("{}) {}".format(idx + 1, element))
+        i = input("enter number: ")
+        if 0 < int(i) <= len(options):
+            return int(i)
+        else:
+            print("invalid selection, try again.")
